ce96782173420de68efc5f2104cc960edd670979
Vladimir <sciphilib@gmail.com>
2023-04-02 17:36:17 +0700

    stage unstaged

--- day4/task2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-[[ $# -lt 2 ]] && echo "Enter the name of the file to be produced and the time at which it will be run."
-
-echo "ffplay \"${1}\"" | at "$2"
-
--- day4/task4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pids=($(pidof systemd))
-ps -fe | head -1
-for i in ${pids[@]}; do
-    ps -fe | awk -v pid=$i '{if($3 == pid){{print $0}}}'
-done
-
